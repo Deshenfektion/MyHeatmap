9e610a3ccea733e37cdbe57fb87efe49906d86a1
Deshenfektion <deshen.r1502@gmail.com>
2025-04-17 23:03:49 +0200

    Remove eslint becuz speed lols

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Deaktiviert ESLint während des Builds
+  },
 };
 
 export default nextConfig;
