77fcc8541b9a6d6e87fda4fdb2c6f65ebc46c8cc
Deshenfektion <deshen.r1502@gmail.com>
2025-04-27 00:16:24 +0200

    Add GitHub login and session checking before redirecting

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+
 import { login, signup } from "@/app/login/actions";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  // GitHub Login Logik with Supabase Client
+  const handleGitHubLogin = async () => {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${window.location.origin}/`, // Dynamische Weiterleitung zur Startseite
+      },
+    });
+    if (error) console.error("Error logging in with GitHub:", error.message);
+  };
+
+  // Session-Überprüfung nach der Weiterleitung
+  useEffect(() => {
+    const checkSession = async () => {
+      const supabase = await createClient();
+      const { data, error } = await supabase.auth.getSession();
+      if (data.session) {
+        // Benutzer ist authentifiziert, leite zur Startseite weiter
+        window.location.href = "/";
+      }
+    };
+
+    checkSession();
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md rounded-lg shadow-md p-8 border border-white">
@@ -45,6 +75,14 @@ export default function LoginPage() {
               className="mt-2 w-full py-2 px-4 rounded-md border border-emerald-600 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2"
             >
               Sign up
+            </button>
+          </div>
+          <div className="mt-6">
+            <button
+              onClick={handleGitHubLogin}
+              className="w-full py-2 px-4 rounded-md border border-gray-800 bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2"
+            >
+              Sign in with GitHub
             </button>
           </div>
         </form>
